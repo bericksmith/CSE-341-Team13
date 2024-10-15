bab2e4743c3d6aa75709057647bb400487dbf9fc
bericksmith <bericksmith@gmail.com>
2024-10-15 14:57:30 -0700

    moved authentication to middleware

--- routes/users.js
+++ routes/users.js
@@ -2,22 +2,15 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const { validateUser, validateUserUpdate, validationResult } = require('../middleware/validation');
+const { ensureAuthenticated } = require('../middleware/authentication');
 
-const ensureAuthenticated = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    res.status(401).send('Unauthorized');
-};
-
+// Public routes
 router.get('/', userController.getUsers);
-
 router.get('/:id', userController.getUserById);
 
+// Protected routes
 router.post('/', ensureAuthenticated, validateUser, validationResult, userController.createUser);
-
 router.put('/:id', ensureAuthenticated, validateUserUpdate, validationResult, userController.updateUser);
-
 router.delete('/:id', ensureAuthenticated, userController.deleteUser);
 
 module.exports = router;
