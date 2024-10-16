ba3363e3879e421fdc0287ee424ffa9681b52770
Lenora Stevens <lenoraRStevens@gmail.com>
2024-10-15 19:35:30 -0500

    added swagger tags

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -2,7 +2,11 @@ const { ObjectId } = require('mongodb');
 
 // Get all Events
 exports.getEvents = async (req, res) => {
-    //#swagger.tags=['Events']
+    /*
+        #swagger.tags=['Events']
+        #swagger.summary = 'GET all events'
+        #swagger.description = 'This endpoint returns a list of all events.'
+    */
     const db = req.app.locals.db;
 
     try {
@@ -16,7 +20,12 @@ exports.getEvents = async (req, res) => {
 
 // Get a event by ID
 exports.getEventById = async (req, res) => {
-    //#swagger.tags=['Events']
+    /*
+        #swagger.tags=['Events']
+        #swagger.summary = 'GET an event by ID'
+        #swagger.description = 'This endpoint returns a single event based on the provided ID.'
+        #swagger.parameters['id'] = { description: 'Event ID', required: true }
+    */
     const db = req.app.locals.db;
     const { id } = req.params;
 
@@ -38,7 +47,24 @@ exports.getEventById = async (req, res) => {
 
 // Create a new event
 exports.createEvent = async (req, res) => {
-    //#swagger.tags=['Events']
+    /*
+        #swagger.tags=['Events']
+        #swagger.summary = 'CREATE a new event (OAuth required)'
+        #swagger.description = 'This endpoint creates a new event with the provided details.'
+        #swagger.parameters['body'] = { 
+            in: 'body',
+            description: 'Event data',
+            required: true,
+            schema: {
+                $name: 'Fun Event',
+                $location: 'Where',
+                $date: '2024-10-29',
+                $time: '08:00 PM',
+                $venue: 'Facility Name'
+            }
+        }
+        #swagger.security = [{ "OAuth2": [] }]
+    */
     const db = req.app.locals.db;
     const { name, location, date, time, venue } = req.body;
 
@@ -59,31 +85,42 @@ exports.createEvent = async (req, res) => {
     }
 };
 
-// Update a event by ID
+// Update an event by ID
 exports.updateEvent = async (req, res) => {
-    //#swagger.tags=['Events']
+     /*
+        #swagger.tags=['Events']
+        #swagger.summary = 'UPDATE an event by ID (OAuth required)'
+        #swagger.description = 'This endpoint updates an event based on the provided ID and data.'
+        #swagger.parameters['id'] = { 
+            description: 'Event ID',
+            required: true,
+            type: 'string'
+        }
+        #swagger.parameters['body'] = {
+            in: 'body',
+            description: 'Updated event data',
+            schema: {
+                name: 'Fun Event',
+                location: 'Where',
+                date: 'YYYY-MM-DD',
+                time: 'hh:mm AM/PM (12-hour format)',
+                venue: 'Name of Facility'
+            } 
+        }
+        #swagger.security = [{ "OAuth2": [] }]
+    */
     const db = req.app.locals.db;
     const { id } = req.params;
+    const updateData = req.body;
     
     try {
         if (!ObjectId.isValid(id)) {
             return res.status(400).json({ message: 'Invalid event ID format' });
         }
 
-        const eventId = ObjectId.createFromHexString(req.params.id);
-
-        const updateData = {
-            $set: {
-                name: req.body.name,
-                location: req.body.location,
-                date: req.body.date,
-                time: req.body.time,
-                venue: req.body.venue
-            }
-        };
-
         const result = await db.collection('events').updateOne(
-            { _id: eventId }, updateData
+            { _id: new ObjectId(id) },
+            { $set: updateData }
         );
 
         if (result.matchedCount === 0) {
@@ -97,9 +134,15 @@ exports.updateEvent = async (req, res) => {
     }
 };
 
-// Delete a event by ID
+// Delete an event by ID
 exports.deleteEvent = async (req, res) => {
-    //#swagger.tags=['Events']
+     /*
+        #swagger.tags=['Events']
+        #swagger.summary = 'DELETE an event by ID (OAuth required)'
+        #swagger.description = 'This endpoint deletes an event based on the provided ID.'
+        #swagger.parameters['id'] = { description: 'Event ID', required: true }
+        #swagger.security = [{ "OAuth2": [] }]
+    */
     const db = req.app.locals.db;
     const { id } = req.params;
 
