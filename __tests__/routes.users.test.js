7a007a1ecaea5ea5d26b24a1e14367b8f3880713
bericksmith <bericksmith@gmail.com>
2024-10-22 11:36:02 -0700

    added supertest module for jest testing

---
+++ __tests__/routes.users.test.js
@@ -0,0 +1,43 @@
+const request = require('supertest');
+const express = require('express');
+const userRouter = require('../routes/users');
+const userController = require('../controllers/userController');
+
+const app = express();
+app.use(express.json());
+app.use('/users', userRouter);
+
+jest.mock('../controllers/userController');
+
+describe('User Routes - GET Endpoints', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should call getUsers controller on GET /users', async () => {
+    userController.getUsers.mockImplementation((req, res) => {
+      res.status(200).json({ users: [{ fname: 'John', lname: 'Doe' }] });
+    });
+
+    const res = await request(app).get('/users');
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('users');
+    expect(userController.getUsers).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call getUserById controller on GET /users/:id', async () => {
+    userController.getUserById.mockImplementation((req, res) => {
+      res.status(200).json({ user: { fname: 'Jane', lname: 'Smith' } });
+    });
+
+    const res = await request(app).get('/users/1');
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('user');
+    expect(userController.getUserById).toHaveBeenCalledTimes(1);
+
+    const req = userController.getUserById.mock.calls[0][0];
+    expect(req.params).toEqual({ id: '1' });
+  });
+});
